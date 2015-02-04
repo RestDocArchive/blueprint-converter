5cdaf69c161b4beadac4562d385707bf9066b142
Thorsten Hoeger <thorsten.hoeger@tullius-walden.com>
2015-02-04 23:24:12 +0100

    progress

--- src/main/java/org/restdoc/blueprint/converter/Starter.java
+++ src/main/java/org/restdoc/blueprint/converter/Starter.java
@@ -1,20 +1,27 @@
 package org.restdoc.blueprint.converter;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.lang.CharEncoding;
-import org.apache.http.HttpResponse;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
 import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.tools.generic.DateTool;
 import org.apache.velocity.tools.generic.MathTool;
+import org.restdoc.api.ParamDefinition;
+import org.restdoc.api.ParamValidation;
 import org.restdoc.api.RestDoc;
+import org.restdoc.api.RestResource;
 import org.restdoc.api.util.RestDocParser;
-
-import de.taimos.httputils.WS;
 
 /**
  * Copyright 2014 Hoegernet<br>
@@ -26,12 +33,22 @@ import de.taimos.httputils.WS;
 public class Starter {
 	
 	public static void main(String[] args) throws IOException {
-		if (args.length != 1) {
-			System.err.println("usage: restdoc2blueprint <url>");
+		InputStream is = System.in;
+		if (args.length == 1) {
+			is = new FileInputStream(new File(args[0]));
 		}
-		String url = args[0];
 		
-		RestDoc doc = Starter.fetchDoc(url);
+		String restdoc;
+		try (BufferedReader read = new BufferedReader(new InputStreamReader(is))) {
+			StringBuilder sb = new StringBuilder();
+			while (read.ready()) {
+				sb.append(read.readLine());
+			}
+			restdoc = sb.toString();
+		}
+		
+		RestDoc doc = RestDocParser.parseString(restdoc);
+		Starter.cleanPaths(doc);
 		
 		Velocity.setProperty(RuntimeConstants.RESOURCE_LOADER, "class");
 		Velocity.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
@@ -55,12 +72,25 @@ public class Starter {
 		}
 	}
 	
-	private static RestDoc fetchDoc(String url) throws IOException {
-		HttpResponse res = WS.url(url).options();
-		if (WS.isStatusOK(res)) {
-			String string = WS.getResponseAsString(res);
-			return RestDocParser.parseString(string);
+	private static void cleanPaths(RestDoc doc) {
+		for (RestResource rr : doc.getResources()) {
+			String path = rr.getPath();
+			Pattern pattern = Pattern.compile("\\{.*?\\}");
+			Matcher matcher = pattern.matcher(path);
+			while (matcher.find()) {
+				String segment = matcher.group();
+				if (segment.indexOf(':') == -1) {
+					continue;
+				}
+				String param = segment.substring(1, segment.indexOf(':'));
+				path = path.replace(segment, "{" + param + "}");
+				ParamDefinition paramDefinition = rr.getParams().get(param);
+				if (paramDefinition != null) {
+					ParamValidation val = new ParamValidation("matches", segment.substring(segment.indexOf(':') + 1, segment.length() - 1));
+					paramDefinition.getValidations().add(val);
+				}
+			}
+			rr.setPath(path);
 		}
-		throw new IOException("invalid HTTP code: " + WS.getStatus(res));
 	}
 }
